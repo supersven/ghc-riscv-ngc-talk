078f59ff608e2f52543ef82819846334109d1f95
Sven Tennie <sven.tennie@gmail.com>
2025-05-17 09:16:19 +0200

    Add nodejs to flake

--- flake.nix
+++ flake.nix
@@ -5,15 +5,17 @@
 
   outputs = inputs:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: inputs.nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import inputs.nixpkgs {
-          inherit system;
-          overlays = [ inputs.self.overlays.default ];
-        };
-      });
-    in
-    {
+      supportedSystems =
+        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (system:
+          f {
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [ inputs.self.overlays.default ];
+            };
+          });
+    in {
       overlays.default = final: prev: rec {
         nodejs = prev.nodejs;
         yarn = (prev.yarn.override { inherit nodejs; });
